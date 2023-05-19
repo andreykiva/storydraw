00bbfbba0b2bea7e705d2a543a1f9b4e9568f8d6
Andrey <57412446+podmost777@users.noreply.github.com>
2023-05-19 05:28:29 +0300

    ForYou page & Video setup

---
+++ client/src/components/ForYouVideo/ForYouVideo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './ForYouVideo.module.css';
+
+const ForYouVideo = () => {
+	return (
+		<div className={styles.ForYouVideo}>
+			ForYouVideo
+		</div>
+	);
+};
+
+export default ForYouVideo;
--- client/src/pages/ForYou/ForYou.jsx
+++ client/src/pages/ForYou/ForYou.jsx
@@ -1,11 +1,86 @@
 import React from 'react';
+import styles from './ForYou.module.css';
+import ForYouVideo from '../../components/ForYouVideo/ForYouVideo';
+
+const testVideos = [
+	{
+		id: 1,
+		userId: '123',
+		username: 'andrey.kiva',
+		userTitle: 'hey man',
+		userImage: '',
+		video: '',
+		description: 'today was a good day',
+		musicId: '123',
+		tags: ['song', 'video', 'day'],
+		likes: 100013,
+		comments: 28,
+		share: 10,
+	},
+	{
+		id: 2,
+		userId: '234',
+		username: 'andrey.kiva',
+		userTitle: 'hey man',
+		userImage: '',
+		video: '',
+		description: 'today was a good day',
+		musicId: '123',
+		tags: ['song', 'video', 'day'],
+		likes: 100013,
+		comments: 28,
+		share: 10,
+	},
+	{
+		id: 3,
+		userId: '345',
+		username: 'andrey.kiva',
+		userTitle: 'hey man',
+		userImage: '',
+		video: '',
+		description: 'today was a good day',
+		musicId: '123',
+		tags: ['song', 'video', 'day'],
+		likes: 100013,
+		comments: 28,
+		share: 10,
+	},
+	{
+		id: 4,
+		userId: '456',
+		username: 'andrey.kiva',
+		userTitle: 'hey man',
+		userImage: '',
+		video: '',
+		description: 'today was a good day',
+		musicId: '123',
+		tags: ['song', 'video', 'day'],
+		likes: 100013,
+		comments: 28,
+		share: 10,
+	},
+	{
+		id: 5,
+		userId: '567',
+		username: 'andrey.kiva',
+		userTitle: 'hey man',
+		userImage: '',
+		video: '',
+		description: 'today was a good day',
+		musicId: '123',
+		tags: ['song', 'video', 'day'],
+		likes: 100013,
+		comments: 28,
+		share: 10,
+	},
+];
 
 const ForYou = () => {
-	return (
-		<div>
-			For You
-		</div>
-	);
+	return <div className={styles.ForYou}>
+		{testVideos.map((video) => (
+			<ForYouVideo />
+		))}
+	</div>;
 };
 
 export default ForYou;
