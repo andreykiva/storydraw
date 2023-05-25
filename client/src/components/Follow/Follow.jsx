8a9e56df9b783dec56aea5540ca4e99a71ac1143
Andrey <57412446+podmost777@users.noreply.github.com>
2023-05-25 05:08:30 +0300

    Following page

--- client/src/components/Follow/Follow.jsx
+++ client/src/components/Follow/Follow.jsx
@@ -4,13 +4,13 @@ import styles from './Follow.module.css';
 import defaultImg from '../../assets/icons/default.svg';
 import previewImg from '../../assets/images/preview.jpg';
 
-const Follow = ({ username, title, image, preview }) => {
+const Follow = ({ username, description, image, preview }) => {
 	return (
 		<div className={styles.Follow}>
 			<img src={preview || previewImg} alt="Preview" className={styles.FollowPreview} />
 			<div className={styles.FollowContent}>
 				<img src={image || defaultImg} alt="Profile picture" className={styles.FollowImg} />
-				<div className={styles.FollowTitle}>{title}</div>
+				<div className={styles.FollowDescr}>{description}</div>
 				<div className={styles.FollowUsername}>{username}</div>
 				<button className={styles.FollowBtn}>Follow</button>
 			</div>
@@ -20,7 +20,7 @@ const Follow = ({ username, title, image, preview }) => {
 
 Follow.propTypes = {
 	username: PropTypes.string,
-	title: PropTypes.string,
+	description: PropTypes.string,
 	image: PropTypes.string,
 	preview: PropTypes.string,
 };
