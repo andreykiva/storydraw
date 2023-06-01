f521211a837750b965ecfb7afd12186edece7cdb
Andrey <57412446+podmost777@users.noreply.github.com>
2023-06-01 05:44:00 +0300

    Tag page

--- client/src/pages/Tag/Tag.jsx
+++ client/src/pages/Tag/Tag.jsx
@@ -1,11 +1,193 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import styles from './Tag.module.css';
+import TagVideo from '../../components/TagVideo/TagVideo';
 
 const testTagInfo = {
 	views: '123k',
-	videos: []
-}
+	videos: [
+		{
+			id: 1,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 2,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 3,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 4,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 5,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 6,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 7,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 8,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 9,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 10,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 11,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 12,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 13,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 14,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 15,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 16,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 17,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 18,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 19,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+		{
+			id: 20,
+			userId: '123',
+			username: 'andrey.kiva',
+			userImage: '',
+			video: '',
+			description: 'today was a good day',
+			tags: ['song', 'video', 'day'],
+		},
+	],
+};
 
 const Tag = () => {
 	let { tag } = useParams();
@@ -20,7 +202,9 @@ const Tag = () => {
 				</div>
 			</div>
 			<div className={styles.TagVideos}>
-				videos
+				{testTagInfo.videos.map((video) => (
+					<TagVideo key={video.id} {...video} />
+				))}
 			</div>
 		</div>
 	);
