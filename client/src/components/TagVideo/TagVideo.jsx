7f239d991493ad7b471e699deffa8011d2280c95
Andrey <57412446+podmost777@users.noreply.github.com>
2023-06-01 05:45:13 +0300

    Added styles & videos

---
+++ client/src/components/TagVideo/TagVideo.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './TagVideo.module.css';
+import defaultImg from '../../assets/icons/default.svg';
+import previewImg from '../../assets/images/preview.jpg';
+
+const TagVideo = (props) => {
+	const { userId, username, userImage, video, description, tags } = props;
+
+	return (
+		<div className={styles.TagVideo}>
+			<div className={styles.TagVideoContent}>
+				<img src={video || previewImg} alt="Video" className={styles.Video} />
+				<div className={styles.UserInfo}>
+					<img src={userImage || defaultImg} alt="Profile picture" className={styles.ProfileImg} />
+					<span className={styles.Username}>{username}</span>
+				</div>
+			</div>
+			<p className={styles.TagVideoDescr}>
+				{description} #{tags.join(' #')}
+			</p>
+		</div>
+	);
+};
+
+export default TagVideo;
