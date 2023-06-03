209b91ba45cec69918a5f2296ecc9e28cd858811
Andrey <57412446+podmost777@users.noreply.github.com>
2023-06-03 05:37:09 +0300

    Added Explore page

---
+++ client/src/components/ExploreVideo/ExploreVideo.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './ExploreVideo.module.css';
+import defaultImg from '../../assets/icons/default.svg';
+import previewImg from '../../assets/images/preview.jpg';
+import LikeIcon from '../../UI/icons/LikeIcon';
+import viewsImg from '../../assets/icons/views.svg';
+
+const ExploreVideo = (props) => {
+	const { userId, username, userImage, video, description, tags, likes, views } = props;
+
+	return (
+		<div className={styles.ExploreVideo}>
+			<div className={styles.ExploreVideoContent}>
+				<img src={video || previewImg} alt="Video" className={styles.Video} />
+				<div className={styles.ViewsInfo}>
+					<img src={viewsImg} alt="Views" className={styles.ViewsIcon} />
+					<span className={styles.Views}>{views}</span>
+				</div>
+			</div>
+			<p className={styles.InfoDescr}>
+				<span className={styles.DescrText}>{description}</span>
+				{tags.map((tag) => (
+					<Link to={'/tag/' + tag} key={tag} className={styles.Tag}>
+						#{tag}
+					</Link>
+				))}
+			</p>
+			<div className={styles.UserInfo}>
+				<img src={userImage || defaultImg} alt="Profile picture" className={styles.ProfileImg} />
+				<span className={styles.Username}>{username}</span>
+				<div className={styles.LikesInfo}>
+					<LikeIcon className={styles.LikeIcon} />
+					<span className={styles.Likes}>{likes}</span>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ExploreVideo;
--- client/src/pages/Explore/Explore.jsx
+++ client/src/pages/Explore/Explore.jsx
@@ -1,9 +1,159 @@
 import React from 'react';
+import styles from './Explore.module.css';
+import drawnImg from '../../assets/icons/brush.svg';
+import pixelImg from '../../assets/icons/pixelated.svg';
+import frameImg from '../../assets/icons/movie-roll.svg';
+import lengthyImg from '../../assets/icons/movie.svg';
+import ExploreVideo from '../../components/ExploreVideo/ExploreVideo';
+
+const exploreCategories = [
+	{
+		name: 'Drawn',
+		icon: drawnImg,
+	},
+	{
+		name: 'Pixel',
+		icon: pixelImg,
+	},
+	{
+		name: 'Frame',
+		icon: frameImg,
+	},
+	{
+		name: 'Lengthy',
+		icon: lengthyImg,
+	},
+];
+
+const testVideos = [
+	{
+		id: 1,
+		userId: '123',
+		username: 'andrey.kiva',
+		userImage: '',
+		video: '',
+		description: 'today was a good day',
+		tags: ['song', 'video', 'day'],
+		likes: 99142,
+		views: 1244,
+	},
+	{
+		id: 2,
+		userId: '123',
+		username: 'andrey.kiva',
+		userImage: '',
+		video: '',
+		description: 'today was a good day',
+		tags: ['song', 'video', 'day'],
+		likes: 99142,
+		views: 1244,
+	},
+	{
+		id: 3,
+		userId: '123',
+		username: 'andrey.kiva',
+		userImage: '',
+		video: '',
+		description: 'today was a good day',
+		tags: ['song', 'video', 'day'],
+		likes: 99142,
+		views: 1244,
+	},
+	{
+		id: 4,
+		userId: '123',
+		username: 'andrey.kiva',
+		userImage: '',
+		video: '',
+		description: 'today was a good day',
+		tags: ['song', 'video', 'day'],
+		likes: 99142,
+		views: 1244,
+	},
+	{
+		id: 5,
+		userId: '123',
+		username: 'andrey.kiva',
+		userImage: '',
+		video: '',
+		description: 'today was a good day',
+		tags: ['song', 'video', 'day'],
+		likes: 99142,
+		views: 1244,
+	},
+	{
+		id: 6,
+		userId: '123',
+		username: 'andrey.kiva',
+		userImage: '',
+		video: '',
+		description: 'today was a good day',
+		tags: ['song', 'video', 'day'],
+		likes: 99142,
+		views: 1244,
+	},
+	{
+		id: 7,
+		userId: '123',
+		username: 'andrey.kiva',
+		userImage: '',
+		video: '',
+		description: 'today was a good day',
+		tags: ['song', 'video', 'day'],
+		likes: 99142,
+		views: 1244,
+	},
+	{
+		id: 8,
+		userId: '123',
+		username: 'andrey.kiva',
+		userImage: '',
+		video: '',
+		description: 'today was a good day',
+		tags: ['song', 'video', 'day'],
+		likes: 99142,
+		views: 1244,
+	},
+	{
+		id: 9,
+		userId: '123',
+		username: 'andrey.kiva',
+		userImage: '',
+		video: '',
+		description: 'today was a good day',
+		tags: ['song', 'video', 'day'],
+		likes: 99142,
+		views: 1244,
+	},
+	{
+		id: 10,
+		userId: '123',
+		username: 'andrey.kiva',
+		userImage: '',
+		video: '',
+		description: 'today was a good day',
+		tags: ['song', 'video', 'day'],
+		likes: 99142,
+		views: 1244,
+	},
+];
 
 const Explore = () => {
 	return (
-		<div>
-			Explore
+		<div className={styles.Explore}>
+			<div className={styles.ExploreCategories}>
+				{exploreCategories.map((category) => (
+					<div className={styles.ExploreCategory} key={category.name}>
+						<img src={category.icon} alt={category.name} className={styles.CategoryIcon} />
+						<span>{category.name}</span>
+					</div>
+				))}
+			</div>
+			<div className={styles.ExploreVideos}>
+				{testVideos.map((video) => (
+					<ExploreVideo key={video.id} {...video} />
+				))}
+			</div>
 		</div>
 	);
 };
