6dda4ad04efbd4591d5525609930d91f88371555
Andrey <57412446+podmost777@users.noreply.github.com>
2023-06-09 05:55:21 +0300

    Search page created

---
+++ client/src/pages/Search/Search.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect } from 'react';
+import { useSearchParams, Routes, Route, NavLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import styles from './Search.module.css';
+import { search } from '../../store/reducers/search/searchSlice';
+import SearchTop from '../../components/SearchTop/SearchTop';
+import SearchAccounts from '../../components/SearchAccounts/SearchAccounts';
+import SearchVideos from '../../components/SearchVideos/SearchVideos';
+
+const Search = () => {
+	const [searchParams] = useSearchParams();
+	const searchParam = searchParams.get('q') || '';
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(search(searchParam));
+	}, [searchParam]);
+
+	const links = [
+		{
+			title: 'Best',
+			to: `/search?q=${searchParam}`,
+		},
+		{
+			title: 'Accounts',
+			to: `/search/user?q=${searchParam}`,
+		},
+		{
+			title: 'Video',
+			to: `/search/video?q=${searchParam}`,
+		},
+	];
+
+	return (
+		<div className={styles.Search}>
+			<div className={styles.SearchTypes}>
+				{links.map((link) => (
+					<NavLink
+						key={link.title}
+						to={link.to}
+						end
+						className={(isActive) => styles.SearchType + (isActive.isActive ? ` ${styles.ActiveType}` : '')}
+					>
+						{link.title}
+					</NavLink>
+				))}
+			</div>
+			<div className={styles.SearchContent}>
+				<Routes>
+					<Route path="/" element={<SearchTop />} />
+					<Route path="/user" element={<SearchAccounts />} />
+					<Route path="/video" element={<SearchVideos />} />
+				</Routes>
+			</div>
+		</div>
+	);
+};
+
+export default Search;
