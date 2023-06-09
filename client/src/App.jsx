61b5711367e80aced3f4dcd57cff4085a630d835
Andrey <57412446+podmost777@users.noreply.github.com>
2023-06-09 05:55:45 +0300

    Search page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import SignIn from './pages/SignIn/SignIn';
 import Layout from './hoc/Layout/Layout';
 import ForYou from './pages/ForYou/ForYou';
 import Following from './pages/Following/Following';
 import Explore from './pages/Explore/Explore';
 import Tag from './pages/Tag/Tag';
+import Search from './pages/Search/Search';
 
 export const App = () => {
 	return (
@@ -15,6 +15,9 @@ export const App = () => {
 					<Route path="/tag/:tag" element={<Tag />} />
 					<Route path="/following" element={<Following />} />
 					<Route path="/explore" element={<Explore />} />
+					<Route path="/search" element={<Search />}>
+						<Route path=":type" element={<Search />} />
+					</Route>
 					<Route path="/" exact element={<ForYou />} />
 					<Route path="*" element={<Navigate to="/" />} />
 				</Routes>
