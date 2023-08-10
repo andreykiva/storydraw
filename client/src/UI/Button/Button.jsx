ca8b3576dabba58c50e01ecc1fe8225815f4fc8e
Andrey <57412446+podmost777@users.noreply.github.com>
2023-08-10 06:04:22 +0300

    Removed deleted files from project

--- client/src/UI/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Button.module.css';
-
-const Button = ({ className, children }) => {
-	return <button className={[styles.Button, className].join(' ')}>{children}</button>;
-};
-
-export default Button;
