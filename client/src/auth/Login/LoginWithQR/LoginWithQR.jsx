2b51193f76a7b99bbdef9dd6ef28f9459eb9aadb
Andrey <57412446+podmost777@users.noreply.github.com>
2023-08-30 06:00:34 +0300

    create LoginWithQR page

--- client/src/auth/Login/LoginWithQR/LoginWithQR.jsx
+++ client/src/auth/Login/LoginWithQR/LoginWithQR.jsx
@@ -4,7 +4,14 @@ import styles from './LoginWithQR.module.css';
 const LoginWithQR = () => {
 	return (
 		<div className={styles.LoginWithQR}>
-			LoginWithQR
+			<h3 className={styles.Title}>Log in with QR code</h3>
+			<div className={styles.QRcodeWrapper}>
+				<div className={styles.QRcode}></div>
+			</div>
+			<div className={styles.QRcodeInfo}>
+				<p className={styles.InfoStep}>1. Scan with your mobile device's camera</p>
+				<p className={styles.InfoStep}>2. Confirm login or sign up</p>
+			</div>
 		</div>
 	);
 };
