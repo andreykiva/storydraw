e2b34eccaa758529ac7c8e77c8d2cb172d41344b
Andrey <57412446+podmost777@users.noreply.github.com>
2023-09-25 22:08:35 +0300

    add FormData & FormErrors types

---
+++ client/src/types/Auth.d.ts
@@ -0,0 +1,22 @@
+import Country from './Country';
+
+export type FormData = {
+	phone: string;
+	login: string;
+	email: string;
+	code: string;
+	password: string;
+	country: Country;
+	birthMonth: string;
+	birthDay: string;
+	birthYear: string;
+	sendTrends: boolean;
+};
+
+export type FormErrors = {
+	login: string;
+	email: string;
+	phone: string;
+	code: string;
+	password: string;
+};
