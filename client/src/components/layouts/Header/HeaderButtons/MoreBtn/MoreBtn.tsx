023fd2c7dfc383e6b99a8856bfe4a51bb37542cc
Andrey <57412446+podmost777@users.noreply.github.com>
2023-11-10 02:23:19 +0200

    remove MoreMenu from MoreBtn

--- client/src/components/layouts/Header/HeaderButtons/MoreBtn/MoreBtn.tsx
+++ client/src/components/layouts/Header/HeaderButtons/MoreBtn/MoreBtn.tsx
@@ -1,38 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import styles from './MoreBtn.module.css';
-import languageIcon from '@/assets/icons/language.svg';
-import helpIcon from '@/assets/icons/help.svg';
-import darkModeIcon from '@/assets/icons/mode-dark.svg';
 import moreIcon from '@/assets/icons/more.svg';
 import defaultImg from '@/assets/icons/default.svg';
+import MoreMenu from './MoreMenu/MoreMenu';
 
 type MoreBtnProps = {
 	isAuth: boolean;
 };
-
-const loggedInUserLinks = [
-	{
-		title: 'View profile',
-		icon: defaultImg,
-		to: '/',
-	},
-	{
-		title: 'Favorites',
-		icon: defaultImg,
-		to: '/',
-	},
-	{
-		title: 'Get Coins',
-		icon: defaultImg,
-		to: '/',
-	},
-	{
-		title: 'Settings',
-		icon: defaultImg,
-		to: '/',
-	},
-];
 
 const MoreBtn = ({ isAuth }: MoreBtnProps) => {
 	return (
@@ -42,34 +16,7 @@ const MoreBtn = ({ isAuth }: MoreBtnProps) => {
 			) : (
 				<img src={moreIcon} alt="More" className={styles.MoreDots} />
 			)}
-			<div className={styles.MoreMenu}>
-				<div className={styles.MenuTriangle}></div>
-				{isAuth &&
-					loggedInUserLinks.map((link) => (
-						<Link to={link.to} className={styles.MoreMenuItem}>
-							<img src={link.icon} alt={link.title} className={styles.ItemIcon} />
-							<span className={styles.ItemText}>{link.title}</span>
-						</Link>
-					))}
-				<div className={styles.MoreMenuItem}>
-					<img src={languageIcon} alt="Language" className={styles.ItemIcon} />
-					<span className={styles.ItemText}>English</span>
-				</div>
-				<a className={styles.MoreMenuItem}>
-					<img src={helpIcon} alt="Help" className={styles.ItemIcon} />
-					<span className={styles.ItemText}>Help</span>
-				</a>
-				<div className={styles.MoreMenuItem}>
-					<img src={darkModeIcon} alt="Dark Mode" className={styles.ItemIcon} />
-					<span className={styles.ItemText}>Dark Mode</span>
-				</div>
-				{isAuth && (
-					<div className={[styles.MoreMenuItem, styles.Logout].join(' ')}>
-						<img src={languageIcon} alt="Language" className={styles.ItemIcon} />
-						<span className={styles.ItemText}>Log out</span>
-					</div>
-				)}
-			</div>
+			<MoreMenu className={styles.MoreMenu} isAuth={isAuth} />
 		</button>
 	);
 };
