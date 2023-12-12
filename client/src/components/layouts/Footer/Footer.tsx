2cf567ce41b82ac503a19c5e6ede6543f2de2fe2
Andrey <57412446+podmost777@users.noreply.github.com>
2023-12-12 03:42:55 +0200

    replace 2023 -> 2024

--- client/src/components/layouts/Footer/Footer.tsx
+++ client/src/components/layouts/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Footer.module.css';
 
 const Footer = () => {
-	return <footer className={styles.Footer}>© 2023 StoryDraw</footer>;
+	return <footer className={styles.Footer}>© 2024 StoryDraw</footer>;
 };
 
 export default Footer;
