ee26308ce26a4e4718ccfbd7dbc6363662f84e45
Andrey <57412446+podmost777@users.noreply.github.com>
2023-12-12 03:51:32 +0200

    create individual components for buttons & remove the shared component ActionBtn

--- client/src/components/layouts/Header/HeaderButtons/ActionButtons/ActionButtons.tsx
+++ client/src/components/layouts/Header/HeaderButtons/ActionButtons/ActionButtons.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import styles from './ActionButtons.module.css';
-import ActionBtn from './ActionBtn/ActionBtn';
-import sendImg from '@/assets/icons/send.svg';
-import chatImg from '@/assets/icons/chat.svg';
+import MessagesBtn from './MessagesBtn/MessagesBtn';
+import InboxBtn from './InboxBtn/InboxBtn';
 
 const ActionButtons = () => {
 	return (
 		<div className={styles.ActionButtons}>
-			<ActionBtn promptText="Messages" to="/">
-				<img src={sendImg} alt="Messages" />
-			</ActionBtn>
-			<ActionBtn promptText="Inbox" to="/">
-				<img src={chatImg} alt="Inbox" />
-			</ActionBtn>
+			<MessagesBtn />
+			<InboxBtn />
 		</div>
 	);
 };
