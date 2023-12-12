8b040552bf2f233bd8998ce2a7d70e88f306404e
Andrey <57412446+podmost777@users.noreply.github.com>
2023-12-12 03:42:28 +0200

    create Notifications component

---
+++ client/src/components/Notifications/Notifications.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Notifications.module.css';
+
+const Notifications = () => {
+	
+	return (
+		<div className={styles.Notifications}>
+			Notifications
+		</div>
+	);
+};
+
+export default Notifications;
