b74c9920369be4b719a05d8b1ac32f9c4ec2db03
Andrey <57412446+podmost777@users.noreply.github.com>
2023-12-12 03:53:37 +0200

    move li into a separate component

--- client/src/components/layouts/Sidebar/Nav/Nav.tsx
+++ client/src/components/layouts/Sidebar/Nav/Nav.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import styles from './Nav.module.css';
-import { NavLink } from 'react-router-dom';
 import LikeIcon from '@/components/ui/icons/LikeIcon';
 import PersonIcon from '@/components/ui/icons/PersonIcon';
 import ExploreIcon from '@/components/ui/icons/ExploreIcon';
+import NavItem from './NavItem/NavItem';
 
 const links = [
 	{
 		title: 'For You',
 		to: '/',
-		iconComponemt: <LikeIcon className={styles.NavLinkIcon} />,
+		iconComponemt: <LikeIcon />,
 	},
 	{
 		title: 'Following',
 		to: '/following',
-		iconComponemt: <PersonIcon className={styles.NavLinkIcon} />,
+		iconComponemt: <PersonIcon />,
 	},
 	{
 		title: 'Explore',
 		to: '/explore',
-		iconComponemt: <ExploreIcon className={styles.NavLinkIcon} />,
+		iconComponemt: <ExploreIcon />,
 	},
 ];
 
@@ -28,17 +28,9 @@ const Nav = () => {
 		<nav className={styles.Nav}>
 			<ul>
 				{links.map((link) => (
-					<li key={link.title} className={styles.NavItem}>
-						<NavLink
-							to={link.to}
-							className={(isActive) =>
-								styles.NavLink + (isActive.isActive ? ` ${styles.ActiveLink}` : '')
-							}
-						>
-							{link.iconComponemt}
-							<span>{link.title}</span>
-						</NavLink>
-					</li>
+					<NavItem key={link.title} title={link.title} to={link.to}>
+						{link.iconComponemt}
+					</NavItem>
 				))}
 			</ul>
 		</nav>
