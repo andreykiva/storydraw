c501dd2a63012380a7e1814545fec49a61391087
Andrey <57412446+podmost777@users.noreply.github.com>
2023-12-23 06:57:08 +0200

    add title and basic styles

--- client/src/components/Notifications/Notifications.tsx
+++ client/src/components/Notifications/Notifications.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './Notifications.module.css';
+import HTag from '../ui/HTag/HTag';
 
 const Notifications = () => {
 	
 	return (
 		<div className={styles.Notifications}>
-			Notifications
+			<div className={styles.NotificationsTriangle}></div>
+			<HTag tag='h4' className={styles.Title}>Notifications</HTag>
 		</div>
 	);
 };
