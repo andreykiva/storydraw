417df2de7b774b7e7d733216d7473377284eab94
Andrey <57412446+podmost777@users.noreply.github.com>
2023-12-29 06:54:16 +0200

    add notifications reducer

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,11 +1,13 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import searchReducer from '@/features/search/searchSlice';
 import authReducer from '@/features/auth/authSlice';
+import notificationsReducer from '@/features/notifications/notificationsSlice';
 import { searchAPI } from '@/services/search/searchService';
 
 const rootReducer = combineReducers({
 	search: searchReducer,
 	auth: authReducer,
+	notifications: notificationsReducer,
 	[searchAPI.reducerPath]: searchAPI.reducer,
 });
 
