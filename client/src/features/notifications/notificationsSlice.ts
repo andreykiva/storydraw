1810fe248bece6ef80e70f912fdb296b229426b8
Andrey <57412446+podmost777@users.noreply.github.com>
2023-12-29 06:53:40 +0200

    create notifications slice

---
+++ client/src/features/notifications/notificationsSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+type NotificationsState = {
+	isNotificationsOpen: boolean;
+	notificationsCategory: 'all' | 'likes' | 'comments' | 'mentionsTags' | 'followers';
+};
+
+const initialState: NotificationsState = {
+	isNotificationsOpen: true,
+	notificationsCategory: 'followers',
+};
+
+export const notificationsSlice = createSlice({
+	name: 'notifications',
+	initialState,
+	reducers: {
+		openNotificationsModal: (state) => {
+			state.isNotificationsOpen = true;
+		},
+		closeNotificationsModal: (state) => {
+			state.isNotificationsOpen = false;
+		},
+		changeNotificationsCategory: (state, action) => {
+			state.notificationsCategory = action.payload;
+		},
+	},
+});
+
+export const { openNotificationsModal, closeNotificationsModal, changeNotificationsCategory } =
+	notificationsSlice.actions;
+
+export const selectNotificationsModalStatus = (state: { notifications: NotificationsState }) =>
+	state.notifications.isNotificationsOpen;
+export const selectNotificationsCategory = (state: { notifications: NotificationsState }) =>
+	state.notifications.notificationsCategory;
+
+export default notificationsSlice.reducer;
