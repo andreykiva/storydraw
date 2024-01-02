6d951b4d34aab0eb667e31a3c0fbb059b6f08011
Andrey <57412446+podmost777@users.noreply.github.com>
2024-01-02 05:28:42 +0200

    create Comment component (basic template)

---
+++ client/src/components/Notifications/Comment/Comment.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Comment.module.css';
+
+const Comment = () => {
+	return (
+		<div className={styles.Comment}>
+			Comment
+		</div>
+	);
+};
+
+export default Comment;
