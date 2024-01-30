2f331b596ca0b73688e6b0c0b1a70f246c803adb
Andrey <57412446+podmost777@users.noreply.github.com>
2024-01-30 05:03:49 +0200

    create types for notifications

---
+++ client/src/types/Notification.d.ts
@@ -0,0 +1,51 @@
+import type User from './User';
+
+type ParentComment = {
+	id: string;
+	user: Pick<User, 'id' | 'title'>;
+	text: string;
+};
+
+type StoryImages = {
+	image: string;
+};
+
+export type CommentNotification = {
+	id: string;
+	type: 'comment';
+	text: string;
+	date: Date;
+	parentComment?: ParentComment;
+	story: {
+		id: string;
+		preview: StoryImages;
+	};
+	user: Pick<User, 'id' | 'username' | 'title' | 'image'>;
+};
+
+export type FollowNotification = {
+	id: string;
+	type: 'follow';
+	date: Date;
+	user: Pick<User, 'id' | 'username' | 'title' | 'image'> & {
+		isFollowingYou: boolean;
+		isFollowedByYou: boolean;
+	};
+};
+
+type LikedUser = Pick<User, 'id' | 'username' | 'title' | 'image'>;
+
+export type LikeNotification = {
+	id: string;
+	type: 'like';
+	date: Date;
+	parentComment?: ParentComment;
+	story: {
+		id: string;
+		preview: StoryImages;
+		author: Pick<User, 'id' | 'username'>;
+	};
+	user?: LikedUser;
+	amount?: number;
+	users?: LikedUser[];
+};
