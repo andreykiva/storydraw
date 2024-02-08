25fb12d6a633b3bc33d5aee4901ad4c828c837c1
Andrey <57412446+podmost777@users.noreply.github.com>
2024-02-08 06:45:58 +0200

    create Messages page

---
+++ client/src/pages/Messages/ChatPanel/ChatPanel.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './ChatPanel.module.css';
+
+const ChatPanel = () => {
+	return <div className={styles.ChatPanel}></div>;
+};
+
+export default ChatPanel;
---
+++ client/src/pages/Messages/MenuPanel/MenuPanel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './MenuPanel.module.css';
+
+const MenuPanel = () => {
+	return (
+		<div className={styles.MenuPanel}>
+			<div className={styles.PanelHeader}></div>
+		</div>
+	);
+};
+
+export default MenuPanel;
