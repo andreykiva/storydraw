298ba05997d173ba15fe40f2b0a9200add996a16
Andrey <57412446+podmost777@users.noreply.github.com>
2024-02-13 06:43:51 +0200

    create Radio ui component

---
+++ client/src/components/ui/inputs/Radio/Radio.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './Radio.module.css';
+
+type RadioProps = {
+	children: React.ReactNode;
+	id: string;
+	name: string;
+	checked: boolean;
+	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const Radio = ({ children, id, name, checked, onChange }: RadioProps) => {
+	return (
+		<div className={styles.Radio}>
+			<input
+				type="radio"
+				id={id}
+				name={name}
+				className={styles.RadioInput}
+				checked={checked}
+				onChange={onChange}
+			/>
+			<label htmlFor={id} className={styles.RadioLabel}>
+				{children}
+			</label>
+		</div>
+	);
+};
+
+export default Radio;
