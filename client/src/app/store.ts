3e583a9e5eed1a01b3bffddf0d7a2d7c621fc07d
Andrey <57412446+podmost777@users.noreply.github.com>
2024-02-29 03:48:03 +0200

    add messagesReducer

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -2,12 +2,14 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import searchReducer from '@/features/search/searchSlice';
 import authReducer from '@/features/auth/authSlice';
 import notificationsReducer from '@/features/notifications/notificationsSlice';
+import messagesReducer from '@/features/messages/messagesSlice';
 import { searchAPI } from '@/services/search/searchService';
 
 const rootReducer = combineReducers({
 	search: searchReducer,
 	auth: authReducer,
 	notifications: notificationsReducer,
+	messages: messagesReducer,
 	[searchAPI.reducerPath]: searchAPI.reducer,
 });
 
