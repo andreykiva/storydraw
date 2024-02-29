0cdc0d380f6568eeeab9572f5c86854afa045920
Andrey <57412446+podmost777@users.noreply.github.com>
2024-02-29 03:50:43 +0200

    create ChatPanelHeader

---
+++ client/src/pages/Messages/ChatPanel/ChatPanelHeader/ChatPanelHeader.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './ChatPanelHeader.module.css';
+import defaultImg from '@/assets/images/default.svg?url';
+import { Link } from 'react-router-dom';
+
+const testChatmate = {
+	id: '91492',
+	username: 'andrii12941295',
+	title: 'andrew172',
+	img: '',
+};
+
+const ChatPanelHeader = () => {
+	return (
+		<div className={styles.ChatPanelHeader}>
+			<Link to={`/@${testChatmate.username}`} className={styles.ChatmateLink}>
+				<div className={styles.ChatmateImgWr}>
+					<img src={testChatmate.img || defaultImg} alt="Profile picture" className={styles.ChatmateImg} />
+				</div>
+				<div className={styles.ChatmateInfo}>
+					<span className={styles.ChatmateTitle}>{testChatmate.title}</span>
+					<span className={styles.ChatmateUsername}>@{testChatmate.username}</span>
+				</div>
+			</Link>
+		</div>
+	);
+};
+
+export default ChatPanelHeader;
