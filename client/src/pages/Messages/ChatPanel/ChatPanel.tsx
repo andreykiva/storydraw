ac8c9d4907fbc92956b10cfdc6e76f8e54cfd0fc
Andrey <57412446+podmost777@users.noreply.github.com>
2024-02-29 03:50:27 +0200

    add ChatPanelHeader

--- client/src/pages/Messages/ChatPanel/ChatPanel.tsx
+++ client/src/pages/Messages/ChatPanel/ChatPanel.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styles from './ChatPanel.module.css';
+import ChatPanelHeader from './ChatPanelHeader/ChatPanelHeader';
+import { selectActiveChatmateId } from '@/features/messages/messagesSlice';
 
 const ChatPanel = () => {
-	return <div className={styles.ChatPanel}></div>;
+	const activeChatmateId = useSelector(selectActiveChatmateId);
+
+	return (
+		<div className={styles.ChatPanel}>
+			{activeChatmateId && <ChatPanelHeader />}
+		</div>
+	);
 };
 
 export default ChatPanel;
