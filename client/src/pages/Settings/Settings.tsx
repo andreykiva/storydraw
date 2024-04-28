7fbc1951a5193305039b4777b8a847ebec5312e3
Andrey <57412446+podmost777@users.noreply.github.com>
2024-04-28 10:27:16 +0300

    add Settings component

---
+++ client/src/pages/Settings/Settings.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Settings.module.css';
+
+const Settings = () => {
+	return <div className={styles.Profile}>Settings:_</div>;
+};
+
+export default Settings;
