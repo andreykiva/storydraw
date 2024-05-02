b705d2b202d492015f991c780eb2cce6fbcfca48
Andrey <57412446+podmost777@users.noreply.github.com>
2024-05-02 09:33:05 +0300

    add 3 settings sections

--- client/src/pages/Settings/SettingsPanel/SettingsPanel.tsx
+++ client/src/pages/Settings/SettingsPanel/SettingsPanel.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import styles from './SettingsPanel.module.css';
+import ManageAccount from './ManageAccount/ManageAccount';
+import Privacy from './Privacy/Privacy';
+import PushNotifications from './PushNotifications/PushNotifications';
 
 const SettingsPanel = () => {
 	return (
 		<div className={styles.SettingsPanel}>
-			
+			<ManageAccount />
+			<Privacy />
+			<PushNotifications />
 		</div>
 	);
 };
