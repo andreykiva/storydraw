ff493332985abb2d425dd3555aaf6580411e1016
Andrey <57412446+podmost777@users.noreply.github.com>
2024-05-06 15:55:35 +0300

    add active section

--- client/src/pages/Settings/Settings.tsx
+++ client/src/pages/Settings/Settings.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Settings.module.css';
 import NavPanel from './NavPanel/NavPanel';
 import SettingsPanel from './SettingsPanel/SettingsPanel';
+import type { SettingsSection } from '@/types/Settings';
 
 const Settings = () => {
+	const [activeSection, setActiveSection] = useState<SettingsSection>('manageAccounts');
+
+	const handleChangeSection = (section: SettingsSection) => {
+		setActiveSection(section);
+	};
+
 	return (
 		<div className={styles.Settings}>
-			<NavPanel />
-			<SettingsPanel />
+			<NavPanel activeSection={activeSection} changeActiveSection={handleChangeSection} />
+			<SettingsPanel activeSection={activeSection} />
 		</div>
 	);
 };
