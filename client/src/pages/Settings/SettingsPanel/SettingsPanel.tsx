fac8d9152611463fd9fb6713d3cf1ea1dac9a3d9
Andrey <57412446+podmost777@users.noreply.github.com>
2024-05-06 16:00:48 +0300

    add scroll to active section & change some styles

--- client/src/pages/Settings/SettingsPanel/SettingsPanel.tsx
+++ client/src/pages/Settings/SettingsPanel/SettingsPanel.tsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './SettingsPanel.module.css';
 import ManageAccount from './ManageAccount/ManageAccount';
 import Privacy from './Privacy/Privacy';
 import PushNotifications from './PushNotifications/PushNotifications';
+import type { SettingsSection } from '@/types/Settings';
 
-const SettingsPanel = () => {
+type SettingsPanelProps = {
+	activeSection: SettingsSection;
+};
+
+const SettingsPanel = ({ activeSection }: SettingsPanelProps) => {
+	const manageAccountRef = useRef<HTMLDivElement>(null);
+	const privacyRef = useRef<HTMLDivElement>(null);
+	const pushNotificationsRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		switch (activeSection) {
+			case 'manageAccounts':
+				manageAccountRef.current.scrollIntoView({ behavior: 'smooth' });
+				break;
+			case 'privacy':
+				privacyRef.current.scrollIntoView({ behavior: 'smooth' });
+				break;
+			case 'pushNotifications':
+				pushNotificationsRef.current.scrollIntoView({ behavior: 'smooth' });
+				break;
+			default:
+				break;
+		}
+	}, [activeSection]);
+
 	return (
 		<div className={styles.SettingsPanel}>
-			<ManageAccount />
-			<Privacy />
-			<PushNotifications />
+			<div ref={manageAccountRef}>
+				<ManageAccount />
+			</div>
+			<div ref={privacyRef}>
+				<Privacy />
+			</div>
+			<div ref={pushNotificationsRef}>
+				<PushNotifications />
+			</div>
 		</div>
 	);
 };
