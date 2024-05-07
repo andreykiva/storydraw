a836a3319edbec848d4342d4993fe582e1f5c4a7
Andrey <57412446+podmost777@users.noreply.github.com>
2024-05-07 16:24:51 +0300

    add DeleteAccountModal diplaying

--- client/src/pages/Settings/SettingsPanel/ManageAccount/ManageAccount.tsx
+++ client/src/pages/Settings/SettingsPanel/ManageAccount/ManageAccount.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ManageAccount.module.css';
 import settingsSharedStyles from '@/pages/Settings/SettingsSharedStyles.module.css';
 import SettingsSection from '@/pages/Settings/SettingsPanel/SettingsSection/SettingsSection';
 import SettingsSubsection from '@/pages/Settings/SettingsPanel/SettingsSubsection/SettingsSubsection';
 import ArrowIcon from '@/assets/icons/arrow.svg';
+import DeleteAccountModal from './DeleteAccountModal/DeleteAccountModal';
 
 const ManageAccount = () => {
+	const [isDeleteAccountModalOpen, setIsDeleteAccountModalOpen] = useState(false);
 
 	return (
-		<SettingsSection title="Manage account" id='manageAccount'>
+		<SettingsSection title="Manage account" id="manageAccount">
 			<SettingsSubsection title="Account control">
 				<div className={settingsSharedStyles.SettingsItem}>
 					<span className={styles.SettingsItemTitle}>Delete account</span>
-					<div className={styles.DeleteBtn}>Delete</div>
+					<div className={styles.DeleteBtn} onClick={setIsDeleteAccountModalOpen.bind(this, true)}>
+						Delete
+					</div>
 				</div>
 			</SettingsSubsection>
 			<SettingsSubsection title="Account information">
@@ -27,6 +31,9 @@ const ManageAccount = () => {
 						Ukraine
 						<ArrowIcon className={styles.ArrowIcon} alt="Arrow" />
 					</div>
+					{isDeleteAccountModalOpen && (
+						<DeleteAccountModal onClose={setIsDeleteAccountModalOpen.bind(this, false)} />
+					)}
 				</div>
 			</SettingsSubsection>
 		</SettingsSection>
