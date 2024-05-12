0e3f8cc976219f4e6c21849229a06085eadaf197
Andrey <57412446+podmost777@users.noreply.github.com>
2024-05-12 17:14:31 +0300

    rename messages -> chat

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -3,6 +3,7 @@ import searchReducer from '@/features/search/searchSlice';
 import authReducer from '@/features/auth/authSlice';
 import notificationsReducer from '@/features/notifications/notificationsSlice';
 import messagesReducer from '@/features/messages/messagesSlice';
+import reportReducer from '@/features/report/reportSlice';
 import { searchAPI } from '@/services/search/searchService';
 
 const rootReducer = combineReducers({
@@ -10,6 +11,7 @@ const rootReducer = combineReducers({
 	auth: authReducer,
 	notifications: notificationsReducer,
 	messages: messagesReducer,
+	report: reportReducer,
 	[searchAPI.reducerPath]: searchAPI.reducer,
 });
 
