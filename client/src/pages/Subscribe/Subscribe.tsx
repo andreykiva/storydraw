c2c3419a07c291225c9ce5ad4bcfaf9d951cbfaa
Andrey <57412446+podmost777@users.noreply.github.com>
2024-05-14 00:59:13 +0300

    create Subscribe page

---
+++ client/src/pages/Subscribe/Subscribe.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './Subscribe.module.css';
+
+const Subscribe = () => {
+	return <div className={styles.Subscribe}>Subscribe</div>;
+};
+export default Subscribe;
