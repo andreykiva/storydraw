e3197713a6fb6f1871f2fef8a2ff19cd20be7e05
Andrey <57412446+podmost777@users.noreply.github.com>
2024-05-15 01:43:36 +0300

    add scss loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -34,6 +34,10 @@ module.exports = {
 				use: ['style-loader', 'css-loader'],
 			},
 			{
+				test: /\.scss$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
 				test: /\.html$/,
 				use: [
 					{
