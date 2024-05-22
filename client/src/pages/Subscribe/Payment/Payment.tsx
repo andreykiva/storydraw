87252a62837dd16a0760fd25c4c3caf217dade69
Andrey <57412446+podmost777@users.noreply.github.com>
2024-05-22 18:22:54 +0300

    add styles for Payment

--- client/src/pages/Subscribe/Payment/Payment.tsx
+++ client/src/pages/Subscribe/Payment/Payment.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './Payment.module.scss';
 
 const Payment = () => {
-	return <div>Payment</div>;
+	return <div className={styles.Payment}>Payment</div>;
 };
 
 export default Payment;
