b66a9e133ccfec57709cf9424598a5c563d37b3f
Andrey <57412446+podmost777@users.noreply.github.com>
2024-05-22 15:23:24 +0300

    rename MentionsTagsList -> MentionsAndTagsList

---
+++ client/src/components/Notifications/MentionsAndTagsList.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import tagIcon from '@/assets/icons/notifications/tag.svg?url';
+import NotificationsList from './NotificationsList/NotificationsList';
+
+const MentionsAndTagsList = () => {
+	return (
+		<NotificationsList
+			notifications={[]}
+			noNotificationsTitle="Mentions of You"
+			noNotificationsIcon={tagIcon}
+			noNotificationsDescr="When someone mentions you, you'll see it here"
+		/>
+	);
+};
+
+export default MentionsAndTagsList;
