b3b6c25ad419a704ef367f6f5f92ecbf5385e769
Andrey <57412446+podmost777@users.noreply.github.com>
2024-05-29 01:44:49 +0300

    add PAYMENT_FIELD enum

--- client/src/constants/subscription.ts
+++ client/src/constants/subscription.ts
@@ -4,6 +4,17 @@ export const enum SUBSCRIPTION_PLAN {
 }
 
 export const enum TRIAL_INFO_SIZE {
-    SMALL = 'small',
-    LARGE = 'large',
+	SMALL = 'small',
+	LARGE = 'large',
+}
+
+export const enum PAYMENT_METHOD {
+	SAVED_CARD = 'savedCard',
+	ANOTHER_CARD = 'anotherCard',
+}
+
+export const enum PAYMENT_FIELD {
+	CARD_NUMBER = 'cardNumber',
+	EXPIRATION_DATE = 'expirationDate',
+	CVC_CODE = 'cvcCode',
 }
