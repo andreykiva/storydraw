a38b20305c6bf57df577facea2ba683e5471e52f
Andrey <57412446+podmost777@users.noreply.github.com>
2024-05-30 15:42:31 +0300

    add new regexp

--- client/src/utils/regexpUtils.ts
+++ client/src/utils/regexpUtils.ts
@@ -5,14 +5,9 @@ export const phonePattern = /^[0-9]+$/;
 export const codePattern = /^[0-9]+$/;
 export const cardNumberPattern =
 	/^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\d{3})\d{11})$/;
+export const expirationDatePattern = /^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$/;
+export const CVCCodePattern = /^\d{3,4}$/;
 
 export const escapeRegExp = (str: string) => {
 	return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-};
-
-export const formatCardNumber = (value: string) => {
-	return value
-		.replace(/\s?/g, '')
-		.replace(/(\d{4})/g, '$1 ')
-		.trim();
 };
