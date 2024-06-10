885fdfc5acc36df88c56ee49bee1e8b2a771cb7e
Andrey <57412446+podmost777@users.noreply.github.com>
2024-06-10 16:55:42 +0300

    create Textarea component

---
+++ client/src/components/ui/inputs/Textarea/Textarea.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import cn from 'classnames';
+import styles from './Textarea.module.scss';
+import warningIcon from '@/assets/icons/auth/warning.svg?url';
+
+type TextareaProps = React.ComponentProps<'textarea'> & {
+	error: string;
+};
+
+const Textarea = ({ error, ...rest }: TextareaProps) => {
+	return (
+		<div className={cn(styles.FormGroup, error && styles.GroupError)}>
+			<div className={styles.TextareaIcon}>
+				<img src={warningIcon} alt="Warning" className={styles.WarningIcon} />
+			</div>
+			<textarea className={styles.Textarea} {...rest} />
+			<p className={styles.TextareaError}>{error}</p>
+		</div>
+	);
+};
+
+export default Textarea;
