c1459ec609c43f9841b45812652630ae3df7c6e7
Andrey <57412446+podmost777@users.noreply.github.com>
2024-06-10 16:56:20 +0300

    add constants file for Profile

---
+++ client/src/constants/profile.ts
@@ -0,0 +1,11 @@
+export const enum EDIT_PROFILE_FIELD {
+	USERNAME = 'username',
+	NAME = 'name',
+	BIO = 'bio',
+}
+
+export const enum RELATIONS_TYPE {
+	FOLLOWING = 'following',
+	FOLLOWERS = 'followers',
+	FRIENDS = 'friends',
+}
