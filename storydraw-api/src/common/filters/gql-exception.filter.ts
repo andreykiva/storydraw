6a0dedbe4b113b045dfd414419a2280a19183828
Andrey <57412446+podmost777@users.noreply.github.com>
2024-07-05 04:18:18 +0300

    create GqlExceptionFilter

---
+++ storydraw-api/src/common/filters/gql-exception.filter.ts
@@ -0,0 +1,28 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
+import { GqlArgumentsHost } from '@nestjs/graphql';
+import { ApolloError } from 'apollo-server-errors';
+
+@Catch(HttpException)
+export class GqlExceptionFilter implements ExceptionFilter {
+	catch(exception: HttpException, host: ArgumentsHost) {
+		const gqlHost = GqlArgumentsHost.create(host);
+		const ctx = gqlHost.getContext();
+		const request = ctx.req;
+
+		const status = exception.getStatus();
+		const message = exception.message;
+
+		let errors = exception.getResponse() as Record<string, any>;
+
+		if (typeof errors === 'string') {
+			errors = { message: errors };
+		}
+
+		throw new ApolloError(message, status.toString(), {
+			statusCode: status,
+			timestamp: new Date().toISOString(),
+			path: request.url,
+			errors,
+		});
+	}
+}
