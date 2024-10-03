5fe0db99fc5c0b6104bfeb6571f321a67c2c1d6d
Andrey <57412446+podmost777@users.noreply.github.com>
2024-10-04 02:21:52 +0300

    add new constants

--- storydraw-ui/src/constants/pagination.ts
+++ storydraw-ui/src/constants/pagination.ts
@@ -1,3 +1,18 @@
 export const FOR_YOU_STORIES_LIMIT = 5;
+
 export const COMMENTS_LIMIT = 10;
 export const REPLIES_LIMIT = 3;
+
+export const USER_STORIES_LIMIT = 12;
+export const FAVORITE_STORIES_LIMIT = 12;
+export const LIKED_STORIES_LIMIT = 12;
+
+export const FOLLOWING_LIMIT = 8;
+export const FOLLOWERS_LIMIT = 8;
+export const FRIENDS_LIMIT = 8;
+
+export const ALL_NOTIFICATIONS_LIMIT = 8;
+export const LIKES_NOTIFICATIONS_LIMIT = 10;
+export const COMMENTS_NOTIFICATIONS_LIMIT = 8;
+export const MENTIONS_NOTIFICATIONS_LIMIT = 8;
+export const FOLLOWS_NOTIFICATIONS_LIMIT = 10;
