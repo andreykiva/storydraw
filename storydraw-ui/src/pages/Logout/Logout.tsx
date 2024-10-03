f081e8cbbee8bd92efcf2cfe59e400bb2436cabf
Andrey <57412446+podmost777@users.noreply.github.com>
2024-10-04 02:14:30 +0300

    add cache clearing

--- storydraw-ui/src/pages/Logout/Logout.tsx
+++ storydraw-ui/src/pages/Logout/Logout.tsx
@@ -1,18 +1,22 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { useApolloClient } from '@apollo/client';
 import { logout } from '@/features/auth/authSlice';
 import { clearUser } from '@/features/user/userSlice';
 
 const Logout = (): null => {
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
+	const client = useApolloClient();
 
 	useEffect(() => {
-		dispatch(logout());
-		dispatch(clearUser());
-		navigate('/');
-	}, [dispatch, navigate]);
+		client.cache.reset().then(() => {
+			dispatch(logout());
+			dispatch(clearUser());
+			navigate('/');
+		});
+	}, [dispatch, navigate, client]);
 
 	return null;
 };
