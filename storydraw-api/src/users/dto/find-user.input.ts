e5d50f08ae2459f53ca385d04eb5a8b0a45e6ae7
Andrey <57412446+podmost777@users.noreply.github.com>
2024-10-11 04:00:38 +0300

    add documentation

--- storydraw-api/src/users/dto/find-user.input.ts
+++ storydraw-api/src/users/dto/find-user.input.ts
@@ -2,8 +2,16 @@ import { InputType, Field } from '@nestjs/graphql';
 import { IsNotEmpty, IsString, Length, Matches } from 'class-validator';
 import { USERNAME_PATTERN } from 'src/common/constants/regexp.constants';
 
+/**
+ * Input Data Transfer Object for finding a user by username.
+ */
 @InputType()
 export class FindOneByUsernameInput {
+	/**
+	 * The username of the user to be found.
+	 * Must be a non-empty string, between 5 and 24 characters,
+	 * and match the defined username pattern.
+	 */
 	@Field()
 	@IsString()
 	@IsNotEmpty()
--- storydraw-api/src/users/dto/update-user.input.ts
+++ storydraw-api/src/users/dto/update-user.input.ts
@@ -2,8 +2,15 @@ import { InputType, Field } from '@nestjs/graphql';
 import { IsNotEmpty, IsOptional, IsString, Length, Matches, MaxLength } from 'class-validator';
 import { USERNAME_PATTERN } from 'src/common/constants/regexp.constants';
 
+/**
+ * Input Data Transfer Object for updating user information.
+ */
 @InputType()
 export class UpdateUserInput {
+	/**
+	 * The new username for the user (optional).
+	 * Must be a string between 5 and 24 characters and match the username pattern.
+	 */
 	@Field()
 	@IsOptional()
 	@IsString()
@@ -11,12 +18,20 @@ export class UpdateUserInput {
 	@Matches(USERNAME_PATTERN, { message: 'Invalid username format' })
 	username: string;
 
+	/**
+	 * The display name for the user (optional).
+	 * Must be a string between 1 and 30 characters.
+	 */
 	@Field()
 	@IsOptional()
 	@IsString()
 	@Length(1, 30)
 	displayName: string;
 
+	/**
+	 * A brief bio for the user (optional).
+	 * Must be a string with a maximum length of 80 characters.
+	 */
 	@Field()
 	@IsOptional()
 	@IsString()
@@ -24,8 +39,15 @@ export class UpdateUserInput {
 	bio: string;
 }
 
+/**
+ * Input Data Transfer Object for updating a user's username.
+ */
 @InputType()
 export class UpdateUsernameInput {
+	/**
+	 * The new username for the user.
+	 * Must be a non-empty string between 5 and 24 characters and match the username pattern.
+	 */
 	@Field()
 	@IsString()
 	@IsNotEmpty()
