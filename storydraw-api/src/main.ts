08a25b0f5fd39986b3b3a5df0e3a0ee9230adb99
Andrey <57412446+podmost777@users.noreply.github.com>
2024-10-17 05:03:29 +0300

    mini updates

--- storydraw-api/src/main.ts
+++ storydraw-api/src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
 	app.useGlobalFilters(new HttpErrorFilter());
 	app.useGlobalFilters(new GqlExceptionFilter());
 
+	console.log(process.env.PORT);
 	await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
