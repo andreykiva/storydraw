ffacd355a374ccf012b2857d4420a8498a3efab1
Andrey <57412446+podmost777@users.noreply.github.com>
2024-10-22 03:25:12 +0300

    add profile nav item

--- storydraw-ui/src/components/layouts/Sidebar/Nav/Nav.tsx
+++ storydraw-ui/src/components/layouts/Sidebar/Nav/Nav.tsx
@@ -1,27 +1,42 @@
+import { useSelector } from 'react-redux';
 import { ReactComponent as LikeIcon } from '@/assets/icons/like.svg';
 import { ReactComponent as PersonIcon } from '@/assets/icons/person.svg';
 import { ReactComponent as ExploreIcon } from '@/assets/icons/explore.svg';
 import NavItem from './NavItem/NavItem';
-
-const links = [
-	{
-		title: 'For You',
-		to: '/',
-		iconComponent: <LikeIcon />,
-	},
-	{
-		title: 'Following',
-		to: '/following',
-		iconComponent: <PersonIcon />,
-	},
-	{
-		title: 'Explore',
-		to: '/explore',
-		iconComponent: <ExploreIcon />,
-	},
-];
+import defaultImg from '@/assets/images/default.svg';
+import { selectAuth } from '@/features/auth/authSlice';
+import { selectUser } from '@/features/user/userSlice';
 
 const Nav = () => {
+	const isAuth = useSelector(selectAuth);
+	const user = useSelector(selectUser);
+
+	const links = [
+		{
+			title: 'For You',
+			to: '/',
+			iconComponent: <LikeIcon />,
+		},
+		{
+			title: 'Following',
+			to: '/following',
+			iconComponent: <PersonIcon />,
+		},
+		{
+			title: 'Explore',
+			to: '/explore',
+			iconComponent: <ExploreIcon />,
+		},
+	];
+
+	if (isAuth && user) {
+		links.push({
+			title: 'Profile',
+			to: `/@${user.username}`,
+			iconComponent: <img src={user.imageUrl || defaultImg} alt="Profile" />,
+		});
+	}
+
 	return (
 		<nav>
 			<ul>
